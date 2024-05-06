3a5492016f72095eebc7ed1f87e5896b75428757
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-05-06 16:13:33 +0200

    fix migrations

--- database/migrations/2024_04_25_094841_create_trains_table.php
+++ database/migrations/2024_04_25_094841_create_trains_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code')->unsigned();
+            $table->text('information');
+            $table->tinyIntegery('carriages_number')->unsigned();
+            $table->string('on_time');
+            $table->smallInteger('delay')->unsigned();
+            $table->string('cancelled');
+            $table->tinyInteger('platform')->unsigned();
             $table->timestamps();
         });
     }
